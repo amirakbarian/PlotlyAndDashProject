2f78787cf71b8897e2ec990e122a001a7552fdae
amirakbarian <amirh.akbarian@gmail.com>
2020-07-10 19:19:19 +0200

    update firstpython

--- firstPython.py
+++ firstPython.py
@@ -1,3 +1,3 @@
 # this is the first python command line for testing
-a = 'hello my dear i am really enjoying this Git'
+a = 'hello my dear i am really enjoying this Git and I am looking forward to it'
 print(a)
